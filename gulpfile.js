15e47a0810571d1eba10cdd71a4908b6ca02ca84
Andrey Veldymanov <andrey.veldymanov@ladevelopers.com>
2018-09-12 20:24:57 +0300

    github build

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const  imagemin = require('gulp-imagemin');
 const  browserSync = require('browser-sync');
 const  del = require('del');
 
-const distFolder = 'public/dist'; // 'docs', 'public/dist'
+const distFolder = 'docs'; // 'docs', 'public/dist'
 
 
 /**
